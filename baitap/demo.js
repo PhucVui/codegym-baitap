a66d8c344173e07c889f5962b6bb3745a9f639ea
Phuc Vui <phucvui16495@gmail.com>
2020-06-03 17:19:13 +0700

    add new

---
+++ baitap/demo.js
@@ -0,0 +1,4 @@
+let chieucao = +prompt('Nhap chieu cao');
+let chieurong = +prompt('Nhap chieu rong');
+        let dientich = chieucao * chieurong ;
+        alert('dien tich cua hinh chu nhat la ' + dientich );
